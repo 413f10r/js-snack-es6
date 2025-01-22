e1b72288271238c977945f22d4ff5b3542ae5902
413f10r <ale-fioretti@live.it>
2025-01-22 18:09:11 +0100

    start

---
+++ JS/main.js
@@ -0,0 +1,61 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+
+
+
+
+
+
+
+
+/*Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+let falli = []
+
+const squadre = [
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Roma", puntiFatti: 0, falliSubiti: 0 }
+];
+
+
+for (let i = 0; i < squadre.length; i++) {
+
+    squadre[i].puntiFatti = Math.floor(Math.random() * 30) + 1;
+    squadre[i].falliSubiti = Math.floor(Math.random() * 30) + 1;
+
+
+    for (let x in squadre) {
+
+        falli.push(squadre[x].squadre, squadre[x].falliSubiti)
+    }
+
+}
+
+console.log(falli)
+
+
+
+
+
+
+
+/*Snack 3 (Bonus)
+Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con li elementi estratti 
+dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).*/
+
+
+
+
+
+
